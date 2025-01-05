8dcb401c417c01b520c2180138b05af52c2d3672
Barazii <mahmoud.baraziii@gmail.com>
2025-01-05 03:56:49 +0100

    Filled the missing values in the column TEMP as it was causing error in parsing the data json files.

--- scripts/processing.py
+++ scripts/processing.py
@@ -8,17 +8,17 @@ def _save_data_splits(train_data, test_data, pc_base_dir):
     # train split
     path = pc_base_dir / "train"
     path.mkdir(parents=True, exist_ok=True)
-    with open(path / "train_data.json", "wb") as f:
+    with open(path / "train.json", "wb") as f:
         for dic in train_data:
-            f.write(json.dumps(dic).encode("utf-8"))
+            f.write(json.dumps(dic, separators=(",", ":")).encode("utf-8"))
             f.write("\n".encode("utf-8"))
 
     # test split
     path = pc_base_dir / "test"
     path.mkdir(parents=True, exist_ok=True)
-    with open(path / "test_data.json", "wb") as f:
+    with open(path / "test.json", "wb") as f:
         for dic in test_data:
-            f.write(json.dumps(dic).encode("utf-8"))
+            f.write(json.dumps(dic, separators=(",", ":")).encode("utf-8"))
             f.write("\n".encode("utf-8"))
 
 
@@ -34,14 +34,16 @@ def processing(pc_base_dir):
     train_data = [
         {
             "start": str(df["datetime"].min()),
-            "target": df["TEMP"].to_list()[:-int(os.environ["PREDICTION_LENGTH"])],
+            "target": df["TEMP"]
+            .fillna(df["TEMP"].mean())
+            .tolist()[: -int(os.environ["PREDICTION_LENGTH"])],
         }
         for df in datasets
     ]
     test_data = [
         {
             "start": str(df["datetime"].min()),
-            "target": df["TEMP"].to_list(),
+            "target": df["TEMP"].fillna(df["TEMP"].mean()).tolist(),
         }
         for df in datasets
     ]
